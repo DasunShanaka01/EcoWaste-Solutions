a61a06e91fa3f954f46ef2b55d56a5e1739f6e7e
dasun <dasunshanaka2002@gmail.com>
2025-10-05 12:01:19 +0530

    first commit

---
+++ Backend/eco_waste_solutions/src/test/java/com/eco_waste_solutions/eco_waste_solutions/EcoWasteSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eco_waste_solutions.eco_waste_solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoWasteSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
