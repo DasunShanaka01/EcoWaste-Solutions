ce28189f942d412380fda1bfe53d084a8ec4ad3e
Apinaja08 <IT23185098@my.sliit.lk>
2025-10-05 12:08:58 +0530

    e

--- Backend/eco_waste_solutions/src/main/java/com/eco_waste_solutions/eco_waste_solutions/HelloController.java
+++ Backend/eco_waste_solutions/src/main/java/com/eco_waste_solutions/eco_waste_solutions/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-    @RequestMapping("/hello") //gfww
+    @RequestMapping("/hello") //gfwwtttt
     public String hello() {
         return "Hello, World!";
     }
