10196384b8383214d1fe3744bd05cb3ed0ca56fd
dasunp-singersl <dasunp@singersl.com>
2025-10-08 15:24:35 +0530

    Rem

--- Backend/eco_waste_solutions/src/main/java/com/eco_waste_solutions/eco_waste_solutions/EcoWasteSolutionsApplication.java
+++ Backend/eco_waste_solutions/src/main/java/com/eco_waste_solutions/eco_waste_solutions/EcoWasteSolutionsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcoWasteSolutionsApplication {
-	
+	//dwada
 	public static void main(String[] args) {
 		SpringApplication.run(EcoWasteSolutionsApplication.class, args);
 	}
