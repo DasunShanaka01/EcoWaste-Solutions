f53df5cb5ce3ae9675b1f24efa1c5acdec3527ac
dasunp-singersl <dasunp@singersl.com>
2025-10-13 12:20:40 +0530

    Update Navbar background color for improved visibility

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav style={{padding: '10px 20px', background: '#2b6cb0', color: 'white'}}>
+    <nav style={{padding: '10px 20px', background: '#44a915ff', color: 'white'}}>
       <div style={{display: 'flex', gap: '16px', alignItems: 'center'}}>
         <Link to="/" style={{color: 'white', textDecoration: 'none', fontWeight: '700'}}>EcoWaste</Link>
         <Link to="/" style={{color: 'white', textDecoration: 'none'}}>Home</Link>
